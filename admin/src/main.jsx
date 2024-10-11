03c9958d9740a577f1e523281b65b4034b457117
marees-web <mareeswarisudalai10@gmail.com>
2024-10-11 21:40:54 +0530

    add

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import AdminContextProvider from "./context/AdminContext.jsx";
 import DoctorContextProvider from "./context/DoctorContext.jsx";
-import AppContextProvider from "./context/appContext.jsx";
+import AppContextProvider from "./context/AppContext.jsx";
 import { StrictMode } from "react";
 
 createRoot(document.getElementById("root")).render(
