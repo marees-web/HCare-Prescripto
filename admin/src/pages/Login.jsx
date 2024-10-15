f480e3bfb4558b2815914e8065de1b4acb3ef4af
marees-web <mareeswarisudalai10@gmail.com>
2024-10-15 20:25:48 +0530

    add

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -4,6 +4,8 @@ import { AdminContext } from '../context/AdminContext';
 import axios from 'axios'
 import { toast } from 'react-toastify';
 import { DoctorContext } from '../context/DoctorContext';
+import { MdAttachEmail } from "react-icons/md";
+import { TbPasswordUser } from "react-icons/tb";
 
 const Login = () => {
     const [state, setState] = useState('Admin');
@@ -44,23 +46,25 @@ const Login = () => {
 
     
   return (
-    <div className="w-full h-full bg-gradient-to-r from-primary/20 via-primary/50 to-primary/90 rounded  shadow-primary/50" >
-    <form onSubmit={onSubmitHandler} className='min-h-[100vh] flex items-center shadow-3xl shadow-inner' >
-        <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-gray-500 text-sm shadow-lg'>
+    <div  >
+    <form onSubmit={onSubmitHandler} className='min-h-[100vh] flex items-center ' >
+        <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border border-primary rounded-xl text-white text-xl shadow-3xl shadow-inner  bg-white/30'>
             <p className='text-3xl font-bold m-auto'><span className='text-primary'>{state} </span>Login</p>
-            <div className='w-full'>
-                <p>Email</p>
-                <input onChange={(e)=>setEmail(e.target.value)} value={email} className='border border-[#DADADA] rounded w-full p-2 mt-1' type="email" required />
+            <div className='w-full relative'>
+                <p className='text-lg font-medium'>Email</p>
+                <input onChange={(e)=>setEmail(e.target.value)} value={email} className='border outline-none border-[#DADADA] rounded w-full p-2 mt-1' type="email" required />
+                <MdAttachEmail className='absolute right-2 top-[45px] rounded-full bg-primary w-10' />
             </div>
-            <div className='w-full'>
-                <p>Password</p>
-                <input value={password} onChange={(e)=>setPassword(e.target.value)} className='border border-[#DADADA] rounded w-full p-2 mt-1' type="password" required />
+            <div className='w-full relative'>
+                <p className='text-lg font-medium'>Password</p>
+                <input value={password} onChange={(e)=>setPassword(e.target.value)} className='border outline-none border-[#DADADA] rounded w-full p-2 mt-1' type="password" required />
+                <TbPasswordUser className='absolute right-2 top-[45px] bg-primary rounded-full w-10' />
             </div>
             <button className='bg-primary text-white font-semibold w-full py-2 rounded-md transition-all duration-300 hover:scale-105'>Login</button>
             {
                 state === 'Admin' ?
-                <p>Doctor Login ? <span className='text-primary underline cursor-pointer' onClick={()=>setState('Doctor')}>Click here</span></p> 
-                : <p>Admin Login ? <span className='text-primary underline cursor-pointer' onClick={()=>setState('Admin')}>Click here</span></p> 
+                <p className='text-primary text-xl font-medium'>Doctor Login ? <span className='text-white font-semibold text-xl underline cursor-pointer ' onClick={()=>setState('Doctor')}>Click here</span></p> 
+                : <p className='text-primary text-xl font-medium'>Admin Login ? <span className='text-white font-semibold text-xl  underline cursor-pointer' onClick={()=>setState('Admin')}>Click here</span></p> 
             }
         </div>
 
