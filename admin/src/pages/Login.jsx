80c9a713e0a228148d07cf621fe665e19cbcc332
marees-web <mareeswarisudalai10@gmail.com>
2024-10-17 14:57:35 +0530

    add

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React,{useContext, useState} from 'react';
-import {assets} from "../assets/admin/assets"
+
 import { AdminContext } from '../context/AdminContext';
 import axios from 'axios'
 import { toast } from 'react-toastify';
@@ -46,30 +46,27 @@ const Login = () => {
 
     
   return (
-    <div  >
-    <form onSubmit={onSubmitHandler} className='min-h-[100vh] flex items-center ' >
-        <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border border-primary rounded-xl text-white text-xl shadow-3xl shadow-inner  bg-white/30'>
-            <p className='text-3xl font-bold m-auto'><span className='text-primary'>{state} </span>Login</p>
-            <div className='w-full relative'>
-                <p className='text-lg font-medium'>Email</p>
-                <input onChange={(e)=>setEmail(e.target.value)} value={email} className='border outline-none border-[#DADADA] rounded w-full p-2 mt-1' type="email" required />
-                <MdAttachEmail className='absolute right-2 top-[45px] rounded-full bg-primary w-10' />
-            </div>
-            <div className='w-full relative'>
-                <p className='text-lg font-medium'>Password</p>
-                <input value={password} onChange={(e)=>setPassword(e.target.value)} className='border outline-none border-[#DADADA] rounded w-full p-2 mt-1' type="password" required />
-                <TbPasswordUser className='absolute right-2 top-[45px] bg-primary rounded-full w-10' />
-            </div>
-            <button className='bg-primary text-white font-semibold w-full py-2 rounded-md transition-all duration-300 hover:scale-105'>Login</button>
-            {
-                state === 'Admin' ?
-                <p className='text-primary text-xl font-medium'>Doctor Login ? <span className='text-white font-semibold text-xl underline cursor-pointer ' onClick={()=>setState('Doctor')}>Click here</span></p> 
-                : <p className='text-primary text-xl font-medium'>Admin Login ? <span className='text-white font-semibold text-xl  underline cursor-pointer' onClick={()=>setState('Admin')}>Click here</span></p> 
-            }
+    
+    <form onSubmit={onSubmitHandler} className='min-h-[80vh] flex items-center'>
+      <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-white text-sm shadow-2xl'>
+        <p className='text-2xl font-semibold m-auto'><span className='text-primary text-2xl font-bold'>{state}</span> Login</p>
+        <div className='w-full '>
+          <p>Email</p>
+          <input onChange={(e) => setEmail(e.target.value)} value={email} className='border border-[#DADADA] rounded w-full p-2 mt-1' type="email" required />
         </div>
-
+        <div className='w-full '>
+          <p>Password</p>
+          <input onChange={(e) => setPassword(e.target.value)} value={password} className='border border-[#DADADA] rounded w-full p-2 mt-1' type="password" required />
+        </div>
+        <button className='bg-primary text-white w-full py-2 rounded-md text-base'>Login</button>
+        {
+          state === 'Admin'
+            ? <p className='text-primary font-semibold text-lg'>Doctor Login? <span onClick={() => setState('Doctor')} className='text-white underline font-bold text-xl cursor-pointer'>Click here</span></p>
+            : <p className='text-primary font-semibold text-lg'>Admin Login? <span onClick={() => setState('Admin')} className='text-white underline font-bold text-xl cursor-pointer'>Click here</span></p>
+        }
+      </div>
     </form>
-    </div>
+    
     
   )
 }
