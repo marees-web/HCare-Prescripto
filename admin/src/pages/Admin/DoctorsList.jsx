10ff31354963ad3e1fda1927ea9623ec36be0273
marees-web <mareeswarisudalai10@gmail.com>
2024-10-18 12:45:31 +0530

    add

--- admin/src/pages/Admin/DoctorsList.jsx
+++ admin/src/pages/Admin/DoctorsList.jsx
@@ -1,21 +1,30 @@
-import React, { useContext,useEffect } from 'react'
+import React, { useContext,useEffect, useState } from 'react'
 import { AdminContext } from '../../context/AdminContext'
 
 const DoctorsList = () => {
   const {doctors,aToken,getAllDoctors,changeAvailability}=useContext(AdminContext);
 
+  const [currentPage,setCurrentpage]=useState(1);
+  const[docPerPage,setDocPerpage]=useState(5);
   useEffect(() => {
     if(aToken){
       getAllDoctors()
     }
     
   }, [aToken])
+
+  const indexofLastdoc=currentPage * docPerPage;
+  const indexOfFirstDoc=indexofLastdoc - docPerPage;
+  const currentDoc=doctors.slice(indexOfFirstDoc,indexofLastdoc);
+  const totalpages=Math.ceil(doctors.length/docPerPage);
+
+  const paginate=(page)=>setCurrentpage(page)
   return (
-    <div className='m-5 max-h-[90vh] overflow-y-scroll '>
+    <div className='m-5 max-h[100vh] '>
        <p className='mb-3 text-2xl font-medium  bg-primary text-white px-6 py-3 w-full  rounded'>All Doctors</p>
        <div className='w-full flex flex-wrap gap-4 pt-5 gap-y-8'>
         {
-          doctors.map((item,index)=>(
+          currentDoc.map((item,index)=>(
             <div className='border border-black shadow rounded-xl max-w-56 overflow-hidden cursor-pointer group  hover:border-primary hover:shadow-xl' key={index}>
               <img className='max-w-52 max-h-56 group-hover:scale-105 transition-all duration-300' src={item.image} alt="" />
               <div className='p-4 mt-3'>
@@ -30,6 +39,16 @@ const DoctorsList = () => {
             </div>
           ))
         }
+        
+       </div>
+       <div className='flex justify-center mt-10 ml-0 h-50 '>
+         <button className='bg-gray-400 border rounded px-4 py-2 text-black text-lg font-medium hover:bg-primary hover:text-white hover:scale-75 transition-all duration-150' onClick={()=>paginate(1)}>First</button>
+         <button className='bg-gray-400 border rounded px-4 py-2 text-black text-lg font-medium hover:bg-primary hover:text-white hover:scale-75 transition-all duration-150' disabled={currentPage === 1} onClick={()=>paginate(currentPage -1)}>Prev</button>
+         {new Array(totalpages).fill(0).map((_,index)=>{
+          return <button className={currentPage === index+1 ? "bg-primary border rounded px-4 py-2 text-black text-lg font-medium hover:scale-75 transition-all duration-150 " : "bg-gray-400 border rounded px-4 py-2 text-black text-lg font-medium hover:bg-primary hover:text-white hover:scale-75 transition-all duration-150"} onClick={()=>paginate(index + 1)} key={index + 1}>{index + 1}</button>
+         })}
+         <button className='bg-gray-400 border rounded px-4 py-2 text-black text-lg font-medium hover:bg-primary hover:text-white hover:scale-75 transition-all duration-150' disabled={currentPage === totalpages} onClick={()=>paginate(currentPage + 1)}>Next</button>
+         <button className='bg-gray-400 border rounded px-4 py-2 text-black text-lg font-medium hover:bg-primary hover:text-white hover:scale-75 transition-all duration-150' onClick={()=>paginate(totalpages)}>Last</button>
        </div>
     </div>
   )
